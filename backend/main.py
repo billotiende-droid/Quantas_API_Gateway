441ba1ea4d966117f31ff8c79d7a24d24b481814
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-05-05 16:54:25 +0300

    feat: implement database fetch logic for user wallets

--- backend/main.py
+++ backend/main.py
@@ -19,3 +19,23 @@ app.add_middleware (
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# database connection helper
+def get_db_data(user_id: int):
+    conn = sqlite3.connect(DATABSE_PATH)
+    conn.row_factory = sqlite3.Row  # Enable access columns by name
+    try:
+        cursor = conn.cursor()
+
+        # JOIN users and wallets to get all wallet info for the user
+        query = """
+        SELECT users.username as user, wallets.currency, wallets.balance
+        FROM users
+        JOIN wallets ON users.id = wallets.user_id
+        WHERE users.id = ?
+        """
+        cursor.execute(query, (user_id,))
+        rows = cursor.fetchall()
+    finally:
+        conn.close()    
+
